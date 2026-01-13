d6f193f6a60bdd6a4f3a866318f14e9aa8c22ac4
pfox27 <80210760+pfox27@users.noreply.github.com>
2026-01-13 13:37:15 -0500

    update

---
+++ SearchString.go
@@ -0,0 +1,38 @@
+/* A programme to traverse (parse) a string and count the number of 
+ * letters in three separate groups in the alphabet. */
+
+package main
+
+import(
+		"fmt"
+		"os"
+      )
+
+func main(){
+
+	numAtoI, numJtoR, numStoZ, numSpaces, numOther := 0, 0, 0, 0, 0
+	sentence := os.Args[1] 
+	
+	for i:= 1; i < len(sentence); i++{
+		letter := string(sentence[i])
+		
+		switch letter{
+			case "a", "b", "c", "d", "e", "f", "g", "h", "i":
+			numAtoI++
+			case "j", "k", "l", "m", "n", "o", "p", "q", "r":
+			numJtoR++
+			case "s", "t", "u", "v", "w", "x", "y", "z":
+			numStoZ++
+			case " ":
+			numSpaces++
+			default:
+			numOther++
+		}
+	}
+		fmt.Printf("Number of letters from A to I: %d\n", numAtoI)
+		fmt.Printf("Number of letters from J to R: %d\n", numJtoR)
+		fmt.Printf("Number of letters from S to Z: %d\n", numStoZ)
+		fmt.Printf("Number of spaces: %d\n", numSpaces)
+		fmt.Printf("Number of \"other\": %d\n", numOther)
+}
+
